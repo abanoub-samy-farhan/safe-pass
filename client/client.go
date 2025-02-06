3c1b716a6d5e29118d27944d5cdc48c841dc887d
AbanoubAziz <abanoubsamy2341@gmail.com>
2025-02-06 04:19:38 +0200

    feat: redis client setup

---
+++ client/client.go
@@ -0,0 +1,15 @@
+package client
+
+import (
+	"github.com/redis/go-redis/v9"	
+)
+
+func InitiateClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	
+	return client
+}

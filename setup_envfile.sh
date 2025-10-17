38edb22289b19be20a3d2baa091fd673a668bc0b
Abanoub Aziz <abanoubsamy2341@gmail.com>
2025-10-17 17:43:31 +0300

    adding random key generation to the setup_envfile.sh

--- setup_envfile.sh
+++ setup_envfile.sh
@@ -1,9 +1,17 @@
-KEY='2*EUH$@^9t$yGk6gUr8nzcKsBzf%zHbZ' # Your Secret Key
-BACKUP="$HOME/.config/safe-pass" # Don't change this
+KEY=$(head -c 32 /dev/urandom | od -t x1 -N 32 | head -n 2 | cut -d' ' -f2- | tr -d ' ' | paste -s -d '') # Your Secret Key
+HOMEDIR="$HOME/.config/safe-pass"
+BACKUP=$HOMEDIR/backups
 
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script should be run with root user"
+    exit 1
+fi
 
 mkdir -p $BACKUP
-touch $BACKUP/.env
+touch $HOMEDIR/.env
 
-echo "KEY=$KEY" > $BACKUP/.env
-echo "BACKUP=$BACKUP" >> $BACKUP/.env
+echo "KEY=$KEY" > $HOMEDIR/.env
+echo "BACKUP=$BACKUP" >> $HOMEDIR/.env
+
+chmod 440 $HOMEDIR/.env
